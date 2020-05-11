a1c393574099a2acba32fdaa1fa569f2cb8a0698
g-lilian <guolilian98@gmail.com>
2020-05-11 18:24:32 +0800

    Refactor shortbranch code
    Moved code from TextFile into ShortBranch

--- src/branching/ShortBranch.java
+++ src/branching/ShortBranch.java
@@ -1,26 +1,55 @@
 package branching;
 
+import data.TextFile;
 import main.Ui;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * Controls branching which is short (a few lines instead of entire chapters).
  */
-public class ShortBranch {
-    private Scanner scanner;
-    private File txt;
+public class ShortBranch extends TextFile {
+    private String currLine;
 
-    public ShortBranch(Scanner scanner, File txt) {
-        this.scanner = scanner;
-        this.txt = txt;
+    public ShortBranch(File txt, HashMap<String, String> speakerAcronyms, String currLine) {
+        super(txt, speakerAcronyms);
+        this.currLine = currLine;
         processBranch();
     }
 
     private void processBranch() {
-        // Prompt user for their choice
-        String userResponse = Ui.getResponse();
+        int endIdx = currLine.indexOf(')');
+        String[] branches = currLine.substring(endIdx+1).trim().split(" ");
+        String response = Ui.getResponse(); // get player branch response
+        int responseIdx = Ui.checkPlayerResponse(response, branches);
+        while (responseIdx == -1) { // while response is invalid
+            response = Ui.promptTryAgain();
+            responseIdx = Ui.checkPlayerResponse(response, branches);
+        }
+
+        // go to correct branch depending on response
+        String branchName = "";
+        while (!branchName.equals(response)) {
+            currLine = scanner.nextLine();
+            branchName = currLine.trim();
+        }
+
+        // process lines as usual until endbranch is read
+        boolean endBranch;
+        while (true) {
+            endBranch = processLines(1);
+            if (endBranch) break;
+            Ui.getResponse(); // enter to print next line(s)
+        }
+
+        // skip over all irrelevant lines
+        int branchesRemaining = branches.length - responseIdx - 1;
+        while (branchesRemaining > 0) {
+            currLine = scanner.nextLine();
+            if (currLine.equals("endbranch")) branchesRemaining--;
+        }
     }
 
     public Scanner updateScanner() {
--- src/data/TextFile.java
+++ src/data/TextFile.java
@@ -97,36 +97,8 @@ public class TextFile {
 
         switch (keyword) {
         case "branch":
-            //ShortBranch shortbranch = new ShortBranch(txt, speakerAcronyms);
-            String[] branches = currLine.substring(endIdx+1).trim().split(" ");
-            String response = Ui.getResponse(); // get player branch response
-            int responseIdx = Ui.checkPlayerResponse(response, branches);
-            while (responseIdx == -1) { // while response is invalid
-                response = Ui.promptTryAgain();
-                responseIdx = Ui.checkPlayerResponse(response, branches);
-            }
-
-            // go to correct branch depending on response
-            String branchName = "";
-            while (!branchName.equals(response)) {
-                currLine = scanner.nextLine();
-                branchName = currLine.trim();
-            }
-
-            // process lines as usual until endbranch is read
-            boolean endBranch;
-            while (true) {
-                endBranch = processLines(1);
-                if (endBranch) break;
-                Ui.getResponse(); // enter to print next line(s)
-            }
-
-            // skip over all irrelevant lines
-            int branchesRemaining = branches.length - responseIdx - 1;
-            while (branchesRemaining > 0) {
-                currLine = scanner.nextLine();
-                if (currLine.equals("endbranch")) branchesRemaining--;
-            }
+            ShortBranch shortbranch = new ShortBranch(txt, speakerAcronyms, currLine);
+            scanner = shortbranch.updateScanner(); // have to update the file pointer!
             break;
         default:
         }
